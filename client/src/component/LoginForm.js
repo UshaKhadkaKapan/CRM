4840e8116e79fbf927a19ea4a776a34f38d59807
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-02 13:22:23 +1000

    starting redux for login

--- client/src/component/LoginForm.js
+++ client/src/component/LoginForm.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Form.Group className="mb-3 " controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          onChange={handleOnChange}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3 " controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleOnChange}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  );
 };
 
 export default LoginForm;
--- client/src/pages/login-registration/Login.js
+++ client/src/pages/login-registration/Login.js
@@ -1,8 +1,13 @@
 import React from "react";
+import LoginForm from "../../component/LoginForm";
 import MainLayout from "../../layout/MainLayout";
 
 const Login = () => {
-  return <MainLayout>Login</MainLayout>;
+  return (
+    <MainLayout>
+      <LoginForm />
+    </MainLayout>
+  );
 };
 
 export default Login;
