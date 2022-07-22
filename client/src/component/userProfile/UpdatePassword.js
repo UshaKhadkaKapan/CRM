1d6568f069c016b1487bae429d1ce4c41fc48160
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-23 09:10:37 +1000

    using razor for password and confirm password

--- client/src/component/userProfile/UpdatePassword.js
+++ client/src/component/userProfile/UpdatePassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Alert, Form } from "react-bootstrap";
 import CustomInput from "../custom-input/CustomInput";
 
 const initialState = {
@@ -9,15 +9,35 @@ const initialState = {
 };
 export const UpdatePassword = () => {
   const [form, setForm] = useState(initialState);
+  const [error, setError] = useState("");
+  const [disableBtn, setDisableBtn] = useState(true);
 
   const handleOnChange = (e) => {
     let { name, value } = e.target;
+
+    if (name === "password" || name === "confirmPassword") {
+      setError("");
+    }
     console.log(name, value);
 
     setForm({
       ...form,
       [name]: value,
     });
+
+    if (name === "confirmPassword") {
+      const { password } = form;
+
+      password !== value && setError("Password do not match");
+      password.length < 6 &&
+        setError("Password must be longer than 6 character");
+
+      !/[a-z]/.test(password) && setError("Password must contain lowercase");
+      !/[A-Z]/.test(password) && setError("Password must contain lowercase");
+      !/[0-9]/.test(password) && setError("Password must contain lowercase");
+
+      !password && setError("New password must be provided");
+    }
   };
 
   const handleOnSubmit = (e) => {
@@ -47,11 +67,6 @@ export const UpdatePassword = () => {
       type: "password",
       value: form.confirmPassword,
     },
-    {
-      type: "submit",
-      value: "Update Password",
-      className: "btn btn-danger",
-    },
   ];
   return (
     <div className="mt-5">
@@ -61,6 +76,20 @@ export const UpdatePassword = () => {
         {inputFields.map((item, i) => (
           <CustomInput key={i} {...item} onChange={handleOnChange} />
         ))}
+        <Form.Text muted>
+          New Password should contain atLeast one upperCase, lowercase and a
+          number and minimum of 6 characters
+        </Form.Text>
+
+        <Form.Group className="mt-5">
+          <Form.Text variant="text-danger fs-3 fw-bolder">{error}</Form.Text>
+        </Form.Group>
+        <Form.Control
+          type="submit"
+          value="Update Password"
+          className="btn btn-danger"
+          disabled={disableBtn}
+        />
       </Form>
     </div>
   );
