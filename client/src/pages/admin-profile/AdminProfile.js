3aefc2da5cad69becfcbdd1ebc371986080d15df
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-22 14:40:05 +1000

    start working UI part of admin profile

--- client/src/pages/admin-profile/AdminProfile.js
+++ client/src/pages/admin-profile/AdminProfile.js
@@ -2,7 +2,16 @@ import React from "react";
 import AdminLayout from "../../layout/AdminLayout";
 
 const AdminProfile = () => {
-  return <AdminLayout>Admin Profile</AdminLayout>;
+  return (
+    <AdminLayout>
+      <h3>Admin Profile</h3>
+
+      {/* user profile form */}
+      <hr />
+
+      {/* user update profile form */}
+    </AdminLayout>
+  );
 };
 
 export default AdminProfile;
