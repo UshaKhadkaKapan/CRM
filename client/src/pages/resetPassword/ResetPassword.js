06f8a47fa1ac9609a3d1336684378e8462c524ad
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-24 15:03:47 +1000

    OPt for reset password is almost complete

--- client/src/pages/resetPassword/ResetPassword.js
+++ client/src/pages/resetPassword/ResetPassword.js
@@ -6,8 +6,56 @@ import { requestOTP } from "../../helper/axios-helper";
 import MainLayout from "../../layout/MainLayout";
 
 const ResetPassword = () => {
-  const [showForm, setShowForm] = useState("otp");
+  const [showForm, setShowForm] = useState("password");
   const [email, setEmail] = useState("");
+  const [form, setForm] = useState({});
+  const [error, setError] = useState("kwsjjs");
+  const [disableBtn, setDisableBtn] = useState(true);
+
+  const handleOnChange = (e) => {
+    let { name, value } = e.target;
+
+    let hasError = "";
+
+    if (name === "password" || name === "confirmPassword") {
+      setError("");
+
+      !disableBtn && setDisableBtn(true);
+    }
+    console.log(name, value);
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+
+    if (name === "confirmPassword") {
+      const { password } = form;
+
+      if (password !== value) {
+        hasError = "Password do not match";
+      }
+      if (password.length < 6) {
+        hasError = "Password must not be longer than 6 character";
+      }
+      if (!/[a-z]/.test(password)) {
+        hasError = "Password must contain lowercase";
+      }
+      if (!/[A-Z]/.test(password)) {
+        hasError = "Password must contain lowercase";
+        if (!/[0-9]/.test(password)) {
+          hasError = "Password must contain lowercase";
+        }
+
+        if (!password) {
+          hasError = "New password must be provided";
+        }
+      }
+
+      setError(hasError);
+      !hasError && setDisableBtn(false);
+    }
+  };
 
   const handleOnOTPRequest = async (e) => {
     e.preventDefault();
@@ -20,6 +68,12 @@ const ResetPassword = () => {
     status === "success" && setShowForm("password");
     Alert(email);
   };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   const otpRequest = [
     {
       label: "Request OTP",
@@ -52,6 +106,7 @@ const ResetPassword = () => {
       required: true,
     },
   ];
+  console.log(form);
   return (
     <MainLayout>
       <div className="reg-form d-flex justify-content-center mt-5">
@@ -77,9 +132,9 @@ const ResetPassword = () => {
             /> */}
 
             <div
-              className="text-right"
-              onClick={() => setShowForm("otp")}
-              style={{ cursor: "pointer" }}
+            //   className="text-right mt-5"
+            //   onClick={() => setShowForm("otp")}
+            //   style={{ cursor: "pointer" }}
             >
               {" "}
               Request OTP again
@@ -90,23 +145,40 @@ const ResetPassword = () => {
         )}
 
         {showForm === "password" && (
-          <Form>
-            <h3>Forgot Password</h3>
+          <Form onSubmit={handleOnSubmit}>
+            <h3>Reset Password</h3>
             <div className="py-3">Request an OTP to reset your Password</div>
 
             {fields.map((fields, i) => (
-              <CustomInput key={i} {...fields} />
+              <CustomInput key={i} {...fields} onChange={handleOnChange} />
             ))}
+
+            <Form.Text muted>
+              New Password should contain atLeast one upperCase, lowercase and a
+              number and minimum of 6 characters
+            </Form.Text>
+
+            <Form.Group className="mt-5">
+              <Form.Text variant="text-danger fs-3 fw-bolder">
+                {error}
+              </Form.Text>
+            </Form.Group>
 
             <CustomInput
               type="submit"
               className="bth btn-danger"
               value="update password"
+              disabled={disableBtn}
             />
 
-            <div className="text-right"> Request OTP again</div>
-
-            {/* <CustomInput type="submit" className="btn btn-outline-primary" /> */}
+            <div
+              className="text-right"
+              onClick={() => setShowForm("otp")}
+              style={{ cursor: "pointer" }}
+            >
+              {" "}
+              Request OTP again
+            </div>
           </Form>
         )}
       </div>
