d078a02d72019447ed7dfb4b71fcab261da57093
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-06 13:54:03 +1000

    start working on product table

--- client/src/pages/products/Products.js
+++ client/src/pages/products/Products.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import AdminLayout from "../../layout/AdminLayout";
 
 const Product = () => {
   return (
     <AdminLayout>
-      <h1>Product pages</h1>
+      <h1 className="mt-5">Product </h1>
+      <div className="text-end">
+        <Link to="/product/new">
+          <Button variant="primary">
+            <i class="fa-solid fa-plus"></i> Add New Product
+          </Button>
+        </Link>
+      </div>
+      <hr />
+      <div className="product-list">{/* show table component here */}</div>
     </AdminLayout>
   );
 };
