0bddf30b2d94d71bf003c9b3e644ff6f70398d8d
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-25 20:35:49 +1000

    connect to database

---
+++ clientApi/server.js
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+import { mongoConnect } from "./src/dbconfig/dbconfig.js";
+mongoConnect();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "you reach a e-commerce api",
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.status || 404;
+  res.status(status).json({
+    status: "error",
+    message: error.message,
+  });
+});
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server is running at http://localhost:${5000}`);
+});
