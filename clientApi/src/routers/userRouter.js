b4710665a58e57d6b93690aab22c4357a75c7d37
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-26 19:01:50 +1000

    used bcryptjs for encrypted password

--- clientApi/src/routers/userRouter.js
+++ clientApi/src/routers/userRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
+
+import { comparePassword, hashPassword } from "../helper/bcryptHelper.js";
+
 import { ClientRegistrationValidation } from "../middlewares/validationMiddleware.js";
 import { createNewUser } from "../models/clientUser/clientUserModel.js";
 const router = express.Router();
 
 router.post("/", ClientRegistrationValidation, async (req, res, next) => {
   try {
+    req.body.password = hashPassword(req.body.password);
     const result = await createNewUser(req.body);
     console.log(result);
 
@@ -19,6 +23,10 @@ router.post("/", ClientRegistrationValidation, async (req, res, next) => {
       message: "todo get method",
     });
   } catch (error) {
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      error.status = 200;
+      error.message = "email already exist";
+    }
     next(error);
   }
 });
