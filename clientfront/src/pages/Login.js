98325fbe478d46175c15d08682676229c5528b1a
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-26 21:05:20 +1000

    UI of login page is complete

--- clientfront/src/pages/Login.js
+++ clientfront/src/pages/Login.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Input, Form } from "antd";
 
-const Login = () => {
-  return <div>Login</div>;
+const Register = () => {
+  return (
+    <div className="login">
+      <Row gutter={16} className="d-flex align-items-center">
+        <Col lg={24} className="text-left p-5 ">
+          <Form layout="vertical" className="login-form p-5">
+            <h1>Register</h1>
+            <hr />
+
+            <Form.Item type="email" name="email" label="Email" required>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              type="password"
+              name="password"
+              label="Password"
+              required
+            >
+              <Input />
+            </Form.Item>
+
+            <button className="btn1 mt-2 mb-3">Login</button>
+            <br />
+            <Link to="/register">Click here to Register</Link>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
