28235e1df6363e51cc6dc687ed987fd768109458
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-09-30 11:53:49 +1000

    start wrking on reset password page

--- clientfront/src/App.js
+++ clientfront/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmailVerification from "./pages/EmailVerification";
 import Login from "./pages/Login";
+import PasswordReset from "./pages/PasswordReset";
 import Register from "./pages/Register";
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/client-verification" element={<EmailVerification />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ clientfront/src/pages/PasswordReset.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MainLayout from "../MainLayout/MainLayout";
+
+const PasswordReset = () => {
+  return <MainLayout>PasswordReset</MainLayout>;
+};
+
+export default PasswordReset;
